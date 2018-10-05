4feaf19429f68e461ed0e3c32201c9f189032bc1
Changhee Son <ChangheeS@a-t-g.com>
2018-10-05 15:45:38 -0500

    Make announcement

---
+++ src/Announcement.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Announcement.css';
+
+class Announcement extends Component{
+    render(){
+        return (
+            <p><h1 className ="logoName"> Kairos With Me</h1> is coming to YOU!</p>
+        );
+    }
+}
+
+export default Announcement;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Announcement from './Announcement';
 
 class App extends Component {
   render() {
@@ -8,9 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Announcement/>
           <a
             className="App-link"
             href="https://reactjs.org"
